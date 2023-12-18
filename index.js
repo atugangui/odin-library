ab178b22f4c9e344224fde1df44daa15ce7df3da
atugangui <amandatugangui@gmail.com>
2023-12-17 21:53:47 -0500

    Replace Library constructor with Library class

--- index.js
+++ index.js
@@ -1,89 +1,97 @@
-const myLibrary = [
-];
-
 class Book {
   constructor(title, author, read) {
     this.title = title;
     this.author = author;
     this.read = read;
-    this.index = myLibrary.length;
+  }
+
+  get index() {
+    return this._index;
+  }
+  
+  set index(index) {
+    this._index = index;
   }
 
   updateRead() {
     this.read = this.read ? false : true; // Toggle book read status
 
     //Should probably have display logic somewhere else...
-    let card = document.body.querySelector(`[data-book-index='${book.index}']`);
+    let card = document.body.querySelector(`[data-book-index='${this.index}']`);
     let readStatus = card.querySelector(".book-read");
   
     // Switch read status
     readStatus.removeChild(readStatus.firstChild);
-    readStatus.appendChild(document.createTextNode(book.read ? "Read" : "Unread"));
+    readStatus.appendChild(document.createTextNode(this.read ? "Read" : "Unread"));
   
     // Switch read status button text
     let readButton = card.querySelector(".read-button");
-    readButton.textContent = book.read ? "Didn't finish" : "Finished";
+    readButton.textContent = this.read ? "Didn't finish" : "Finished";
   }
 }
 
-function Library() {
-}
+class Library {
+  constructor(){
+    this.collection = [];
+  }
+  
+  addBook(book) {
+    this.collection.push(book);
+    book.index = this.collection.length;
+    this.displayBook(book);
+  }
 
-Library.prototype.addBook = function(book) {
-  myLibrary.push(book);
-  this.displayBook(book);
-}
+  displayBook(book){
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute('data-book-index', book.index);
+  
+    // Fill card with book information
+    let title = document.createElement("p");
+    title.appendChild(document.createTextNode(book.title));
+    title.className="book-title";
+    card.appendChild(title);
+  
+    let author = document.createElement("p");
+    author.appendChild(document.createTextNode(book.author));
+    author.className="book-author";
+    card.appendChild(author);
+  
+    let read = document.createElement("p");
+    read.appendChild(document.createTextNode(book.read ? "Read" : "Unread"));
+    read.className="book-read";
+    card.appendChild(read);
+  
+    // Add a button to remove a book from the library
+    let removeButton = document.createElement("button");
+    removeButton.textContent="Remove";
+    removeButton.className="remove-button";
+    removeButton.addEventListener('click', () =>{
+      this.removeBookDisplay(book);
+    })
+    card.appendChild(removeButton);
+  
+    // Add a button to change the read status of the book
+    let readButton = document.createElement("button");
+    readButton.textContent = book.read ? "Didn't finish" : "Finished!";
+    readButton.className = "read-button"
+    readButton.addEventListener('click', function(e){
+      book.updateRead(book);
+    });
+    card.appendChild(readButton);
+  
+    document.getElementById("card-container").appendChild(card);
+  }
 
-Library.prototype.removeBook = function(book) {
-  myLibrary.splice(book.index, 1);
-  this.removeBookDisplay(book.index);
-}
+  removeBook(book) {
+    this.collection.splice(book.index, 1);
+    this.removeBookDisplay(book);
+  }
 
-Library.prototype.displayBook = (book) => {
-  let card = document.createElement("div");
-  card.classList.add("card");
-  card.setAttribute('data-book-index', book.index);
-
-  // Fill card with book information
-  let title = document.createElement("p");
-  title.appendChild(document.createTextNode(book.title));
-  title.className="book-title";
-  card.appendChild(title);
-
-  let author = document.createElement("p");
-  author.appendChild(document.createTextNode(book.author));
-  author.className="book-author";
-  card.appendChild(author);
-
-  let read = document.createElement("p");
-  read.appendChild(document.createTextNode(book.read ? "Read" : "Unread"));
-  read.className="book-read";
-  card.appendChild(read);
-
-  // Add a button to remove a book from the library
-  let removeButton = document.createElement("button");
-  removeButton.textContent="Remove";
-  removeButton.className="remove-button";
-  removeButton.addEventListener('click', (e) => {
-    library.removeBookDisplay(book.index);
-  })
-  card.appendChild(removeButton);
-
-  // Add a button to change the read status of the book
-  let readButton = document.createElement("button");
-  readButton.textContent = book.read ? "Didn't finish" : "Finished!";
-  readButton.className = "read-button"
-  readButton.addEventListener('click', function(e){
-    book.updateRead(book);
-  });
-  card.appendChild(readButton);
-
-  document.getElementById("card-container").appendChild(card);
-}
-
-Library.prototype.removeBookDisplay = (index) => {
-  let cardContainer = document.getElementById("card-container");
-  cardContainer.removeChild(document.body.querySelector(`[data-book-index='${index}']`));
+  removeBookDisplay = (book) => {
+    let cardContainer = document.getElementById("card-container");
+    cardContainer.removeChild(document.body.querySelector(`[data-book-index='${book.index}']`));
+  }
 }
 
 const library = new Library();
